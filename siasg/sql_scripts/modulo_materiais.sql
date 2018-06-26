8539e1df425b6a007be2f860435cb527c583c7e5
Fernanda Bezerra <f.bezerra@alu.ufc.br>
2018-06-26 18:32:55 -0300

    Adding create tables

--- siasg/sql_scripts/modulo_materiais.sql
+++ siasg/sql_scripts/modulo_materiais.sql
@@ -12,3 +12,29 @@
 --materiais	
 --	Link: http://compras.dados.gov.br/docs/materiais/v1/materiais.html
 
+CREATE TABLE classes (
+codigo integer,
+codigo_grupo integer,
+descricao text,
+);
+
+CREATE TABLE grupos (
+codigo integer,
+descricao text,
+);
+
+CREATE TABLE pdms (
+codigo integer,
+codigo_classe integer,
+descricao text
+);
+
+CREATE TABLE materiais (
+codigo integer,
+descricao text,
+id_classe integer,
+id_grupo integer,
+id_pdm integer,
+status boolean,
+sustentavel boolean,
+);
