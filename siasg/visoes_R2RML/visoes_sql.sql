7fc1b27d16d5d5e66cd3c1d5e71b791585c0d5dd
Matheus Mayron <matheusmayron@gmail.com>
2018-06-27 23:21:37 -0300

    Criação de diretorio para estruturação do projeto

---
+++ siasg/visoes_R2RML/visoes_sql.sql
@@ -0,0 +1,7 @@
+-- Todas as UASGS do Ceara 
+
+-- Todos os Orgaos relacionados as UASGS do Ceara
+
+-- Todos os contratos ligados as UASGS do Ceara
+
+-- Todas as licitações ligadas as uasgs do Ceara
