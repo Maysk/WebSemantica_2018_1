9515b97c68a9abfb54d58f4ad455467d0fabdf75
Matheus Mayron <matheusmayron@gmail.com>
2018-06-28 17:31:04 -0300

    Correção no script cnep.sql

--- cnep/sql_scripts/cnep.sql
+++ cnep/sql_scripts/cnep.sql
@@ -4,26 +4,26 @@
 
 CREATE TABLE cnep (
     tipo_pessoa text,
-    cpf text,
-    cnpj text,
-    nome text,
+    cpf_ou_cnpj text,
+    nome_informado_pelo_sancionador text,
     razao_social text,
     nome_fantasia text,
     numero_processo text,
     tipo_sancao text,
     data_inicio text,
     data_final text,
-    uf_orgao text,
+    orgao_sancionador text,
+    uf_orgao_sancionador text,
     origem_informacao text,
     data_origem text,
     data_publicacao text,
     publicacao text,
     detalhamento text,
+    valor_multa text,
     fundamentacao text,
     descricao_fundamentacao text,
     data_transito text,
-    complemento_orgao text,
-    valor_multa text
+    complemento_orgao text
 );
 
 -- ==================== Inserção dos Dados ====================
