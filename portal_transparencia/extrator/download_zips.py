42e15d9417773b32febb8c0d136a70abcd821097
Matheus Mayron <matheusmayron@gmail.com>
2018-07-04 16:29:46 -0300

    Começo CEIS

--- portal_transparencia/extrator/download_zips.py
+++ portal_transparencia/extrator/download_zips.py
@@ -12,9 +12,9 @@ mesAtual = mesInicial
 
 
 while (anoAtual < anoFinal or (anoAtual == anoFinal and mesAtual <= mesFinal ) ):
-	download_url = "http://www.portaldatransparencia.gov.br/download-de-dados/licitacoes/{}{:02}".format(anoAtual, mesAtual)
+	download_url = "http://www.portaldatransparencia.gov.br/download-de-dados/compras/{}{:02}".format(anoAtual, mesAtual)
 	print("Downloading {}{:02}".format(anoAtual, mesAtual))
-	nome_do_arquivo = "dados/{}{:02}.zip".format(anoAtual, mesAtual)
+	nome_do_arquivo = "dados_contratos/{}{:02}.zip".format(anoAtual, mesAtual)
 	urllib.request.urlretrieve(download_url, nome_do_arquivo)
 	if(mesAtual == 12):
 		anoAtual = anoAtual + 1
